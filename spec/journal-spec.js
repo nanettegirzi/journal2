4ab782f565e4c3cf904b33ccd20562db561c7335
faiza husain and nanette girzi <nanette@>
2018-03-13 14:46:32 -0700

    add testing

--- spec/journal-spec.js
+++ spec/journal-spec.js
@@ -1,22 +1,19 @@
 import { Entry } from './../src/journal.js';
 
-// describe('Entry', function(){
-//
-//   it('should return the count of words in the body', function(){
-//     var testEntry = new Entry("Hello")
-//     expect(testEntry).toEqual(1);
-//   });
-// });
-
 describe('Entry', function() {
 
   it('should return the count of words in the body', function() {
-    var testEntry = new Entry("title", "hello")
-    expect(testEntry.countWords()).toEqual(1)
+    var testEntry = new Entry("title", "hello my friend")
+    expect(testEntry.TotalWords("hello my friend")).toEqual(3)
   });
 
   it('should return the count of vowels in the word', function() {
     var testEntry = new Entry("title", "gracias")
-    expect(testEntry.countVowels()).toEqual(3)
+    expect(testEntry.CountVowels("gracias")).toEqual(3)
+  });
+
+  it('should return the count of consonants in the body', function() {
+    var testEntry = new Entry("title", "hello")
+    expect(testEntry.CountConsonants("hello")).toEqual(3)
   });
 });
